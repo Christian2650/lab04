b589b72bc7405788df749dc51afe71db042895d5
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-17 14:37:58 +0200

    Add files via upload

---
+++ 2_kod_losujacy2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ćw_2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const int n = 10
+            Random random = new Random();
+
+            int licznikLosowan = 0;
+            int iloscLiczbDoWylosowania = n;
+
+            for (int iloscLiczbWylosowanych = 0; iloscLiczbWylosowanych < iloscLiczbDoWylosowania; iloscLiczbWylosowanych++)
+            {
+                int wylosowanaLiczba;
+                wylosowanaLiczba = random.Next(-4,5);
+                wylosowanaLiczba *=2;
+                licznikLosowan++;
+
+                Console.Write(wylosowanaLiczba);
+
+                if (iloscLiczbWylosowanych<iloscLiczbDoWylosowania-1)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.WriteLine("\nLicznik liczb wylosowanych" + licznikLosowan);
+
+        Console.ReadLine();
+    }
+}
