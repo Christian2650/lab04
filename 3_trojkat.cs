559ffbeb4082a21fb8745db2fa4ab08e0f189cc0
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-21 10:07:01 +0200

    Add files via upload

---
+++ 3_trojkat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("Podaj liczbę naturalną n>1: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++) // 1
+                    Console.Write(" ");
+
+                for (j = 1; j <= i; j++)   // 2
+                    Console.Write("X");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
