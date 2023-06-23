b1328bfcdd2d2be400c19d12b69b9520cc24adef
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-23 09:29:15 +0200

    Add files via upload

---
+++ 1_n_liczb_pierwszych.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1liczbypierwsze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int ilosc = int.Parse(Console.ReadLine());
+            int zapisane     = 0;
+            for (int i = 2; zapisane < ilosc; i++)
+            {
+                Boolean sprawdz = CzyPierwsza(i);
+                if (sprawdz == true)
+                {
+                    Console.WriteLine(i);
+                    zapisane++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static Boolean CzyPierwsza(int i)
+        {
+            int j = 2;
+            while (j != i)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+                else if (j % i != 0)
+                {
+                    j++;
+                    continue;
+
+                }
+
+            }
+            return true;
+        }
+    }
+}
